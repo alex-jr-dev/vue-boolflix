f89c7625b704cbe9ff1e8676f5b13a030417d207
alex-jr-dev <geremia.alessandro@icloud.com>
2021-05-14 12:21:23 +0200

    Implemented flags function and start graphic

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@
 // 3. Lingua
 // 4. Voto -> DONE
 
-// 2 - Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome). FIXME:
+// 2 - Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome). -> DONE
 // - Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici) -> DONE
 
 // 3 - In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco. Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
@@ -52,28 +52,25 @@ new Vue({
       this.makeAxiosSearch("tv");
     },
 
-    getFlags() {
+    getFlags(currentMovie) {
       const lang2country = {
-        en: ["gb", "us", "ca"],
-        it: ["it", "ch"],
-        es: ["es", "ar", "pt"],
-        fr: ["fr", "mc", "be"],
-        de: ["de", "at"],
+        en: ["us"],
+        it: ["it"],
+        es: ["es"],
+        fr: ["fr"],
+        de: ["de"],
         ru: ["ru"],
       };
 
       const fallbackFlag = "null";
 
-      const queryLang = this.moviesList.map((element) => {
-        return element.original_language;
-      });
+      const queryLang = currentMovie.original_language;
 
-      //   FIXME:
-      const candidatesCountries = Object.keys(lang2country).includes(queryLang)
-        ? lang2country[myLeng]
+      const candidatesCountries = lang2country[queryLang]
+        ? lang2country[queryLang]
         : [fallbackFlag];
 
-      return candidatesCountries;
+      return candidatesCountries[0];
     },
   },
   computed: {
